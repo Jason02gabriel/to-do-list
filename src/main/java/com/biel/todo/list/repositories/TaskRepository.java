bb8084c62f3e6e75f027d6469a265236d76d7b32
Jason_Gabriel <j.gabrielaraujodasilva@gmail.com>
2024-05-08 09:23:59 -0300

    feat: TaskRepository created

---
+++ src/main/java/com/biel/todo/list/repositories/TaskRepository.java
@@ -0,0 +1,9 @@
+package com.biel.todo.list.repositories;
+
+import com.biel.todo.list.domain.entities.Tasks;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TaskRepository extends JpaRepository<Tasks, Integer> {
+}
